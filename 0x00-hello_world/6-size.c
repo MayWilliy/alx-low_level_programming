10718c1f5889828ea4cdc8c4f2156d846c0a7ff9
MayWilliy <williamsmadebusola@gmail.com>
2023-03-16 21:57:10 -0700

    origin

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,20 +8,19 @@
  */
 int main(void)
 {
-        char m;
-        int n;
-        long int o;
-        long long int x;
-        float p;
-        char output[145];
-        
-		snprintf(output, sizeof(output),
-	 	"Size of a char: %zu byte(s)\n"
-                "Size of an int: %zu byte(s)\n"
-                "Size of a long int: %zu byte(s)\n"
-                "Size of a long long int: %zu byte(s)\n"
-                "Size of a float: %zu byte(s)\n",
-		sizeof(m), sizeof(n), sizeof(o), sizeof(x), sizeof(p));
-
-		return (0);
+	char m;
+	int n;
+	long int o;
+	long long int x;
+	float p;
+	char output[145];
+	
+	snprintf(output, sizeof(output),
+	"Size of a char: %zu byte(s)\n"
+	"Size of an int: %zu byte(s)\n"
+	"Size of a long int: %zu byte(s)\n"
+	"Size of a long long int: %zu byte(s)\n"
+	"Size of a float: %zu byte(s)\n",
+	sizeof(m), sizeof(n), sizeof(o), sizeof(x), sizeof(p));
+	return (0);
 }
