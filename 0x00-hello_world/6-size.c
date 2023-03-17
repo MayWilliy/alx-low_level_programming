963729654f4082ddeaede0ae95f7b29d5bddcc6e
MayWilliy <williamsmadebusola@gmail.com>
2023-03-16 20:07:41 -0700

    origin

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,22 @@
  */
 int main(void)
 {
-
-
+        char m;
+        int n;
+        long int o;
+        long long int x;
+        float p;
+        char output[145];
+	char output[145];
+        
+	snprintf(output, sizeof(output),
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 		printf("Size of an int: %lu byte(s)\n", sizeof(int));
 			printf("Size of a long int: %lu byte(s)\n",sizeof(long int));
 				printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
 					printf("Size of a float: %lu byte\n", sizeof(float));
+				                sizeof(m), sizeof(n), sizeof(o), sizeof(x), sizeof(p));
+					        printf("%.144s", output);
 
 								return (0);
 }
