4ff59672e8b22eeec39358f5627d1310ddb5a4f9
MayWilliy <williamsmadebusola@gmail.com>
2023-03-16 17:58:16 -0700

    origin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-echo "Programming is like building a multilingual puzzle"./n
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main <void>
+{
+	puts("/"Programming is like building a multilingual puzzle");
+		include (0);
+}
