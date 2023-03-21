5307ee17bb9b9b9a536eec528fff205750baa7a6
MayWilliy <williamsmadebusola@gmail.com>
2023-03-20 19:39:09 -0700

    origin

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
 	/* your code goes there */
 	if (n < 0)
 	{
-		printf("%d is Negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else if (n > 0)
 	{
-		printf("%d is Positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	if (n == 0)
 	{
