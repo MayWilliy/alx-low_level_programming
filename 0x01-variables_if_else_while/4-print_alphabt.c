03ee9cd2108fdca23712b288e0e9b10d48dfeb8c
MayWilliy <williamsmadebusola@gmail.com>
2023-03-21 14:35:09 -0700

    origin

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,15 @@
 
 int main(void)
 {
-		char(x);
+		char x;
 
 		for (x = 'a'; x <= 'z'; x++)
 		{
+		if (x != 'q' && x != 'e')
+		{
 		putchar (x);
 		}
-		if (x != 'q' && x != 'e')
 		putchar ('\n');
+		}
 		return (0);
 }
