51c15985db5ce0a3249f4bacce5ac8736b2cdf27
MayWilliy <williamsmadebusola@gmail.com>
2023-03-20 23:31:54 -0700

    origin

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Don't use prinf
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a', : x <= 'z', ++x)
+	{
+		putchar(x = 'A', : x <= 'Z', ++x)
+		putchar(x);
+		putchar('\n');
+	}
+	return (0);
+}
