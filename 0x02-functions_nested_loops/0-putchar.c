3931fc17dbb5f4ea2ff08048b7a0f915885076c5
MayWilliy <williamsmadebusola@gmail.com>
2023-03-21 19:36:50 -0700

    origin

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - nesteed loop and c functions
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar("_putchar");
+	putchar("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+C programming
+nested loop
+C funictions
