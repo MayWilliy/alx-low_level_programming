9ca583582ed9bc56c365bf9b847945c4ddf680b7
MayWilliy <williamsmadebusola@gmail.com>
2023-03-22 16:31:57 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - short description
+ *
+ * Return - return 0 (Success)
+ *
+ */
+int main(void)
+{
+
+	int alphabet;
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	{
+	_putchar(ch);
+	}
+	}
+	_putchar('\n');
+	return (0);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,7 @@
 #define MAIN_H
 int _putchar(char c);
 #endif
+#ifndef MAIN_H
+#define MAIN_H
+void print_alphabet(void);
+#endif
