b96e8f8bd7d351f034d77a077dab2a433f5d0911
MayWilliy <williamsmadebusola@gmail.com>
2023-03-22 17:15:41 -0700

    nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,14 @@
  * Return - return 0 (Success)
  *
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-	_putchar(ch);
+		_putchar(ch);
 	}
 	_putchar('\n');
 	return (0);
