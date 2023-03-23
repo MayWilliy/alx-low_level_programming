72444b7cb2d4be8abca80bd0d35135e0309aac9b
MayWilliy <williamsmadebusola@gmail.com>
2023-03-22 19:07:04 -0700

    nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,14 @@
  */
 void print_alphabet_x10(void)
 {
-		int i, ch;
+			int i, ch;
 
-		for (i = 0; i < 10; i++)
-			{
+			for (i = 0; i < 10; i++)
+				{
 			for (ch = 'a' ; ch <= 'z' ; ch++)
-			{
-					_putchar(ch);
-					}
-							}
-								_putchar('\n');
+				{
+				_putchar(ch)
+				}
+				}
+			_putchar('\n');
 }
