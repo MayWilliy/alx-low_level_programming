46a9945869add7772505c932f4f250324bc53f29
MayWilliy <williamsmadebusola@gmail.com>
2023-03-24 16:49:22 -0700

    doing hard things

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_numbers - print numbers
+ *
+ * Description: print number from 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+}
+	int i;
+
+	for (i = '0', i <= '9', i++)
+	{
+		_putchar (i);
+		{
+		_putchar ('\n');
+		}
+	}
+	return (0);
+{
