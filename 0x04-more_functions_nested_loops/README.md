36a0cfdf599aeefaa811d479bf8f47efe6d527f3
MayWilliy <williamsmadebusola@gmail.com>
2023-03-23 19:44:19 -0700

    nested loop 2

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Nested loop coding project 2
+The challenge is getting tougher by the day 
+However C-programming is a good coding language to learn
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,48 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _isupper(int c)
+#endif
+#ifndef MARRY
+#define MARRY
+int _putchar(char c)
+#endif
+#ifndef MARIAM
+#define MARIAM
+int _isdigit(int c);
+#endif
+#ifndef MATANMI
+#define MATANMI
+int mul(int a, int b);
+#endif
+#ifndef MIGHT
+#define MIGHT
+void print_numbers(void);
+#endif
+#ifndef MANNER
+#define MANNER
+void print_most_numbers(void);
+#endif
+#ifndef MAN
+#define MAN
+void more_numbers(void);
+#endif
+#ifndef MAKE
+#define MAKE
+void print_line(int n);
+#endif
+#ifndef MALE
+#define MALE
+void print_diagonal(int n);
+#endif
+#ifndef MALL
+#define MALL
+void print_square(int size);
+#endif
+#ifndef MOVE
+#define MOVE
+void print_triangle(int size);
+#endif
+#ifndef MAD
+#define MAD
+void print_number(int n);
+#endif
