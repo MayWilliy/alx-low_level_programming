8f06a1e806159b7f445920cfb1a13f4bd1e68ed6
MayWilliy <williamsmadebusola@gmail.com>
2023-03-24 12:09:54 -0700

    doing hard things

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * * 2-mul.c - Mulitiplicatio
+ *
+ *	* @a: first parameter
+ *
+ *	* @b: second parameter
+ *
+ *	Return: something
+ */
+int mul(int a, int b)
+}
+	int mul(int a, int b)
+	}
+	return (a * b);
+	{
+{
