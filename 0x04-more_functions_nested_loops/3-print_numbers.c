7d6382be0f28937cc48e7bdc759edb034b73f8f3
MayWilliy <williamsmadebusola@gmail.com>
2023-03-24 18:32:49 -0700

    doing hard things

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,19 +5,15 @@
  *
  * Description: print number from 0-9
  *
- * Return: Always 0 (Success)
+ * Return: Always (void)
  */
 void print_numbers(void)
 {
-	int i;
+		int i;
 
 		for (i = '0'; i <= '9'; i++)
 		{
-			if (i != 2 && i != 4)
-			{
-				_putchar (i);
-				}
-				_putchar ('\n');
-				}
-			return(0);
+			_putchar (i);
+			}
+			_putchar ('\n');
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,4 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print number 1 - 9
