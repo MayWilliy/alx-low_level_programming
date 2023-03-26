891eaaece6b737ca4e2b1cd0aa59c435a6c36edb
MayWilliy <williamsmadebusola@gmail.com>
2023-03-25 20:13:28 -0700

    debugging error

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,30 @@
-#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        int i;
+	int n;
 
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+	}
+		printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	}
+		printf("%d is zero\n", n);
+	}
+	return (0);
 }
