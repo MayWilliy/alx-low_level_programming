8fd45b56a0b1268df9558014aa35d0b7fe5794a1
MayWilliy <williamsmadebusola@gmail.com>
2023-03-25 21:10:23 -0700

    debugging error

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,34 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-/* more headers goes there */
-
-/**
- * main - Entry point of the program
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	return (0);
-}
-
 #include "main.h"
 
 /**
@@ -38,10 +7,10 @@ int main(void)
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
+	i = 98;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
