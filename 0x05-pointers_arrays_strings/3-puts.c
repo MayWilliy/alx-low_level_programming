43e85ae5e7be0d5c81a7a42152e97d48e94b6a43
MayWilliy <williamsmadebusola@gmail.com>
2023-03-28 19:47:52 -0700

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -12,16 +12,12 @@ void _puts(char *str)
 {
 	int j;
 
-	j = 0;
 	while (j >= 0)
 	{
 		if (str[j] == "\0")
 		{
-			_putchat ("\n");
-			}	
-				break;
-				_putchar (str[j]);
-					j++;
-				}
-
+		_putchar (str[j]);
+			j++;
+			}
+			_putchar ("\n"0;
 }
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+/**
+ * rev_string - print a reverse string
+ *
+ * @s: 
