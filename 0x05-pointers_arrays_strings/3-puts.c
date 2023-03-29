e8922c9187e78f20fb58031c1618c48ef79a5a69
MayWilliy <williamsmadebusola@gmail.com>
2023-03-28 18:17:37 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints a stand out string
+ *
+ * @str: pointer to the string to print
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int j;
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+	_putchar (str[j]);
+		j++;
+	}
+	_putchar ("\n");
+
+}
