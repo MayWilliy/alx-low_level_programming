3541b7a3715c35ed116b2b42043297eaaae12e03
MayWilliy <williamsmadebusola@gmail.com>
2023-04-04 14:51:56 -0700

    0x07

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,14 +11,18 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	unsigned int i, j = 0;
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8; j++)
+		for (i % 8 == 0 && i = 0)
 		{
-			_putchar (a[i][j]);
-		}
+			j = i;
 		_putchar ('\n');
+		}
+
+		_putchar (a[i / 8][i - j]);
+
 	}
+		_putchar ('\n');
 }
