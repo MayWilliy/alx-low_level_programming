fd484d396e30f55dec25f759e365249c6b926e34
MayWilliy <williamsmadebusola@gmail.com>
2023-04-05 21:40:18 -0700

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,13 +12,16 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (x == 0)
-	{
-		return (1);
-	}
 	if (y == 0)
 	{
 		return (1);
 	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
 	return (x * _pow_recursion(x, y - 1));
+	}
 }
