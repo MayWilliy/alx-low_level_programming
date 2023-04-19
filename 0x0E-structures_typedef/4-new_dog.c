d18f03054774e311700eb455776a1ee416d1a575
MayWilliy <williamsmadebusola@gmail.com>
2023-04-18 17:54:07 -0700

    struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog
+ * @name : dup the name of dog into new_dog name
+ * @age: dup the age of dog into new_dog age
+ * @owner: dup the owner into new_dog owner
+ * Return: NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog->name = strdup(name);
+	new_dog->owner = strdup(owner);
+
+	new_dog->age = age;
+
+	return (new_dog);
+}
