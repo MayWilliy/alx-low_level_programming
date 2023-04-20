011f6c044fe7b62460d5995f0f97b7b7f3fcbd89
MayWilliy <williamsmadebusola@gmail.com>
2023-04-19 21:38:37 -0700

    function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,9 +4,17 @@
 /**
  * array_iterator - given as a parameter on each element of an array
  * @size: is the size of an array
+ * @array: the array of size
  * @action: pointer to the function
  * Return: Nothing cause it is void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
 
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
