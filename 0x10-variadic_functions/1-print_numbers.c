a1f48f420e6ca3337b9bf42bdafbff19b150b40b
MayWilliy <williamsmadebusola@gmail.com>
2023-04-20 19:59:59 -0700

    variadic func

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,9 +20,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 	}
-	if (separator != NULL && i != n - 1)
+	if (separator == NULL)
 	{
-		printf("%s", separator);
 	}
 	printf("\n");
 
