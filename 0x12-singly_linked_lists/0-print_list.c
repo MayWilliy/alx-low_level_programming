c7269a4ed1a69b040abe690b76d6132b9d5278f6
MayWilliy <williamsmadebusola@gmail.com>
2023-04-28 00:03:28 -0700

    linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: pointer to the element
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 1;
+
+		if (h == NULL)
+			return (0);
+
+		while (h->next != NULL)
+		{
+			if (h->str == NULL)
+			printf("[0](nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		node_count =+ 1;
+		}
+		printf("[%d] %s\n", h->len, h->str);
+	return (node_count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Data structure can be stored in two way array and linked list
