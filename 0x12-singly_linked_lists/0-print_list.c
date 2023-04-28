a838bdd53bd4b0480bb4ded36815720713ab72e6
MayWilliy <williamsmadebusola@gmail.com>
2023-04-28 01:24:21 -0700

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,18 +12,15 @@ size_t print_list(const list_t *h)
 {
 	size_t node_count = 1;
 
-		if (h == NULL)
-			return (0);
-
 		while (h->next != NULL)
 		{
 			if (h->str == NULL)
-			printf("[0](nil)\n");
+				printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
-		node_count =+ 1;
+		node_count = +1;
 		}
 		printf("[%d] %s\n", h->len, h->str);
 	return (node_count);
