8471c953ddb6549c3b6659c27a0a5d05cd180746
MayWilliy <williamsmadebusola@gmail.com>
2023-05-01 18:59:16 -0700

    more linked list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head : double pointer to the node
+ * @n: number of nodes
+ * Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *add_nodeint, *temp;
+
+	add_nodeint = malloc(sizeof(listint_t));
+		if (add_nodeint == NULL)
+		return (NULL);
+
+		add_nodeint->n = n;
+		add_nodeint->next = NULL;
+
+		if (*head == NULL)
+		{
+			*head = add_nodeint;
+			return (add_nodeint);
+		}
+		else 
+		{
+		temp = *head;
+		while (temp->next != NULL)
+		temp = temp->next;
+		temp->next = add_nodeint;
+		}
+
+		return (add_nodeint);
+
+}
