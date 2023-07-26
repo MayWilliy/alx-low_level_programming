de0c5478301c4336b27f35449a68d2f73ebc60ae
MayWilliy <williamsmadebusola@gmail.com>
2023-07-26 14:52:46 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
 	unsigned int num = 0;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
-	for (; , *b != '\0'; b++)
+	for (1 = 0; b[i]; i++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 
-		num << = 1;
-		if (*b == '1')
-			num + = 1,
+		num = 2 * num + (b[i] - '0');
 	}
 	return (num);
 }
